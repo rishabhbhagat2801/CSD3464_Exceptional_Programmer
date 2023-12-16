c84c0e94952ec837d9fba9c68b42966dc359ff3a
Rishabh <rishabhbhagat2801@gmail.com>
2023-12-15 20:54:37 -0500

    Edited ExpenseIncomesTracker class

--- ExpenseTrackerbyHardcoreCoders/src/ExpenseTracker/ExpensesIncomesTracker.java
+++ ExpenseTrackerbyHardcoreCoders/src/ExpenseTracker/ExpensesIncomesTracker.java
@@ -1,2 +1,39 @@
-public class ExpensesIncomesTracker {
+package ExpenseTracker;
+
+/**
+ Project Name : Expense Tracker
+
+ Student Name's:
+ Rishabh Vinay Bhagat (C0892204)            Aatman Rajyaguru (C0895289)
+ Karamveer Singh (C0893963)                 Hyder Ali(C0893786)
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ExpensesIncomesTracker extends JFrame {
+
+    private final ExpenseTrackerTableModel tableModel;
+    private final JTable table;
+    private final JTextField dateField;
+    private final JTextField descriptionField;
+    private final JTextField amountField;
+    private final JComboBox<String> typeCombobox;
+    private final JButton addButton;
+    private final JLabel balanceLabel;
+    private double balance;
+    public ExpensesIncomesTracker(){
+
+        UIManager.put("TextField.foreground", Color.BLACK);
+        UIManager.put("TextField.background", Color.WHITE);
+        UIManager.put("TextField.caretForeground", Color.RED);
+
+        UIManager.put("ComboBox.foreground", Color.BLUE);
+        UIManager.put("ComboBox.selectionForeground", Color.WHITE);
+        UIManager.put("ComboBox.selectionBackground", Color.BLACK);
+
+    }
+
 }
+
+
