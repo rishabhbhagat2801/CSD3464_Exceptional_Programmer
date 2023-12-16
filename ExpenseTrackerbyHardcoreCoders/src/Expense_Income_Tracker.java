b9198cd00d2bbaf5ce5ecb94800a550d73b88121
Rishabh <rishabhbhagat2801@gmail.com>
2023-12-15 20:44:13 -0500

    changed the structuer

--- ExpenseTrackerbyHardcoreCoders/src/Expense_Income_Tracker.java
+++
@@ -1,2 +0,0 @@
-public class Expense_Income_Tracker {
-}
---
+++ ExpenseTrackerbyHardcoreCoders/src/Expensetracker/Expense_Income_Tracker.java
@@ -0,0 +1,19 @@
+package ExpenseTracker;
+
+/**
+ Project Name : Expense Tracker
+
+ Student Name's:
+ Rishabh Vinay Bhagat (C0892204)            Aatman Rajyaguru (C0895289)
+ Karamveer Singh (C0893963)                 Hyder Ali(C0893786)
+ */
+
+public class Expense_Income_Tracker {
+
+    public static void main(String[] args) {
+
+        new ExpensesIncomesTracker().setLocationRelativeTo(null);
+
+    }
+
+}
